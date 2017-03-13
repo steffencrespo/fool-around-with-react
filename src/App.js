eadb594cf668cadb76b5ca7a59d28515d71aef17
Leonardo Steffen <leosteffen84@gmail.com>
2017-03-13 12:26:42 -0700

    playing with Higher Order Components - creating one, passing a class component and a stateless funcl component to see the difference

--- src/App.js
+++ src/App.js
@@ -1,38 +1,59 @@
 import React from 'react';
 
-class App extends React.Component {
+const HOC = (InnerComponent) => class extends React.Component {
   constructor() {
     super();
-    this.state = {items: []}
+    this.state = {count: 0}
+  }
+
+  update() {
+    this.setState({count: this.state.count +1})
   }
 
   componentWillMount() {
-    fetch('http://swapi.co/api/people/?format=json')
-      .then( response => response.json() )
-      .then( ({ results: items }) => this.setState({items}) )
+    console.log('will mount');
   }
-
-  filter(e) {
-    this.setState({filter: e.target.value})
-  }
-
   render() {
-    let items = this.state.items;
-    if (this.state.filter) {
-      items = items.filter( item =>
-        item.name.toLowerCase()
-        .includes(this.state.filter.toLowerCase()))
-    }
-    return(
-        <div>
-          <input type="text"
-          onChange={this.filter.bind(this)} />
-          {items.map( item => <Person key={item.name} person={item} />)}
-        </div>
+    return (
+      <InnerComponent
+        {...this.props}
+        {...this.state}
+        update={this.update.bind(this)}
+      />
     )
   }
 }
 
-const Person = (props) => <h4>{props.person.name}</h4>
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Button>button</Button>
+        <hr/>
+        <LabelHOC>label</LabelHOC>
+      </div>
+    )
+  }
+}
+
+const Button = HOC((props) =>
+  <button onClick={props.update}>
+  {props.children} - {props.count}
+  </button>)
+
+class Label extends React.Component {
+  componentWillMount() {
+    console.log('label will mount');
+  }
+  render() {
+    return (
+      <label onMouseMove={this.props.update}>
+      {this.props.children} - {this.props.count}
+      </label>
+    )
+  }
+}
+
+const LabelHOC = HOC(Label)
 
 export default App
